7540730a62f88de26b9f8bf6c0c87c0cb2ca7f41
Daniel <thedezz360@gmail.com>
2024-03-12 11:26:53 +0100

    try 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site : "https://thedezz360.github.io",
-	base: "/portfolio-minimalista"
 });
